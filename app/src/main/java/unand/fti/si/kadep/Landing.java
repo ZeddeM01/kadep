4493951b374645e3ac10b20a531ed307ebb45c02
raidhqtrnnd <90620651+raidhqtrnnd@users.noreply.github.com>
2022-09-22 13:49:36 +0700

    bismillah 3

---
+++ app/src/main/java/unand/fti/si/kadep/Landing.java
@@ -0,0 +1,14 @@
+package unand.fti.si.kadep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Landing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+    }
+}
