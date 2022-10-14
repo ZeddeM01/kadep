a6a5dd3218ac54e53d70a2c7662aa82ce5ad19f4
Novya Lira Natasya <73506155+tasyalira@users.noreply.github.com>
2022-10-14 08:36:08 +0700

    Add files via upload

--- app/src/main/java/unand/fti/si/kadep/MainActivity.java
+++ app/src/main/java/unand/fti/si/kadep/MainActivity.java
@@ -1,14 +1,52 @@
-package unand.fti.si.kadep;
+package com.develovit.sita;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerAdapter recyclerAdapter;
+    private RecyclerView recyclerView;
+    private ArrayList<DataRecyclerView> listdata;
+    private GridLayoutManager layoutmanager;
+    ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        layoutmanager=new GridLayoutManager(getApplicationContext(),1);
+        layoutmanager.setOrientation(GridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutmanager);
+        listdata = new ArrayList<DataRecyclerView>();
+        recyclerAdapter = new RecyclerAdapter(this,listdata);
+        recyclerView.setAdapter(recyclerAdapter);
+
+        back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        show();
+    }
+
+    void show(){
+
+        listdata.add(new DataRecyclerView("Pengembangan Aplikasi Perpustakaan","Silvia","Yudi Hermandi, S.Kom, M.Ti",getResources().getDrawable(R.drawable.profil1)));
+        recyclerAdapter.notifyDataSetChanged();
+
+        listdata.add(new DataRecyclerView("Automatisasi Smart Home","Muhammad A R","Yaman Khaeruzzaman, M.Sc",getResources().getDrawable(R.drawable.profil2)));
+        recyclerAdapter.notifyDataSetChanged();
     }
 }
